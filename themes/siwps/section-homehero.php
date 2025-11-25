adc9023fc1d43e413b4b302907776a7a06e5a3a5
Aaron Gemmill <63611552+gemmillaru@users.noreply.github.com>
2025-11-25 00:23:06 -0500

    pausing for the night with new content added

--- themes/siwps/section-homehero.php
+++ themes/siwps/section-homehero.php
@@ -1,23 +1,28 @@
 <div id="home_hero">
     <div id="home_gallery">
-    <?php get_template_part('component','gallery'); ?>
-
-    
-        
-
+        <?php get_template_part('component', 'gallery'); ?>
     </div>
-
 </div>
 
 <div id="home_info">
     <div class="tab">
-    <?php the_content(); ?>
+        <?php the_content(); ?>
     </div>
 
-    <div class="home_email">
-    <?php get_template_part('component','emailform'); ?>
+    <div class="tab">
+    <?php
+    $research_post = get_page_by_path('the-politics-and-global-economy-page-lab', OBJECT, 'programs');
+    if ($research_post) {
+        echo get_the_post_thumbnail($research_post->ID, 'medium');
+        echo '<h2>' . esc_html(get_the_title($research_post->ID)) . '</h2>';
+    } else {
+        echo '<p>Research post not found.</p>';
+    }
+    ?>
+    </div>
+
+    <div class="tab">
+        <?php get_template_part('component', 'emailform'); ?>
     </div>
 </div>
 <!-- // replace carousel w/ something like this iew-source:https://tympanus.net/Development/AnimatedResponsiveImageGrid/index2.html -->
-
-
