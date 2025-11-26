933773a27dc500efbe3234d0a689c36e0476afea
Aaron Gemmill <63611552+gemmillaru@users.noreply.github.com>
2025-11-26 02:16:00 -0500

    more homepage

--- themes/siwps/section-homehero.php
+++ themes/siwps/section-homehero.php
@@ -9,6 +9,7 @@
         <div class="the_date">
             <?php
             $news_item_id = get_field('sticky_news_item');
+            $about_page = get_page_by_path('about', OBJECT);
             if ($news_item_id) :
                 $news_item = get_post($news_item_id);
               
@@ -16,8 +17,9 @@
                 <h2><?php echo get_the_title($news_item->ID); ?></h2>
                 <?php echo get_the_excerpt($news_item->ID); ?>
             <?php else: ?>
-                <h2><?php echo get_the_title(); ?></h2>
-                <?php echo get_the_excerpt(); ?>
+                <h2><a href="<?php echo get_permalink($about_page->ID); ?>"><?php echo get_the_title($about_page->ID); ?></a></h2>
+            <?php echo get_field('summary', $about_page->ID); ?>
+            <p><a href="<?php echo get_permalink($about_page->ID); ?>">Learn more</a></p>
             <?php endif; ?>
         </div>
 
@@ -28,13 +30,18 @@
         $research_post = get_page_by_path('the-politics-and-global-economy-page-lab', OBJECT, 'programs');
         if ($research_post) :
             ?>
-            <h2><?php echo get_the_title($research_post->ID); ?></h2>
+            <h2><a href="<?php echo get_permalink($research_post->ID); ?>"><?php echo get_the_title($research_post->ID); ?></a></h2>
             <?php
             $the_image = get_field('gallery', $research_post->ID)[0];
             if ($the_image) :
                 ?>
-                <img class="home_info_image" src="<?php echo esc_url($the_image['sizes']['large']); ?>" alt="">
+                <a href="<?php echo get_permalink($research_post->ID); ?>">
+                    <img class="home_info_image" src="<?php echo esc_url($the_image['sizes']['large']); ?>" alt="">
+                </a>
             <?php endif; ?>
+
+            <?php echo get_field('summary', $research_post->ID); ?>
+            <p><a href="<?php echo get_permalink($research_post->ID); ?>">Learn more</a></p>
         <?php else : ?>
             <p>Research post not found.</p>
         <?php endif; ?>
@@ -46,8 +53,9 @@
             $giving_post = get_page_by_path('giving', OBJECT);
             if ($giving_post) :
                 ?>
-                <h2><?php echo get_the_title($giving_post->ID); ?></h2>
-                <?php echo esc_html(get_the_excerpt($giving_post->ID)); ?>
+                <h2><a href="<?php echo get_permalink($giving_post->ID); ?>"><?php echo get_the_title($giving_post->ID); ?></a></h2>
+                <?php echo get_field('summary', $giving_post->ID); ?>
+                <p><a href="<?php echo get_permalink($giving_post->ID); ?>">Learn more</a></p>
             <?php else : ?>
                 <p>Giving post not found.</p>
             <?php endif; ?>
